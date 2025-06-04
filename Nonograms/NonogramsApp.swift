8ece93e475e15feba680f11c3f4fce98d9777cb5
Trevor Behlman <tsbehlman@users.noreply.github.com>
2025-06-04 19:22:43 -0400

    Initial commit

---
+++ Nonograms/NonogramsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NonogramsApp.swift
+//  Nonograms
+//
+//  Created by Trevor Behlman on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct NonogramsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NonogramsTests/NonogramsTests.swift
@@ -0,0 +1,17 @@
+//
+//  NonogramsTests.swift
+//  NonogramsTests
+//
+//  Created by Trevor Behlman on 6/1/25.
+//
+
+import Testing
+@testable import Nonograms
+
+struct NonogramsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
