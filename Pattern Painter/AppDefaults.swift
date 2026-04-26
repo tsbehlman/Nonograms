4fde67af5bcb63ecf727d3aa0c497e9790c8ebe8
Trevor Behlman <tsbehlman@gmail.com>
2026-04-25 23:02:10 -0400

    Lower minimum zoom level

--- Pattern Painter/AppDefaults.swift
+++ Pattern Painter/AppDefaults.swift
@@ -9,7 +9,7 @@ import CoreGraphics
 
 struct AppDefaults {
     static let tileSize: CGFloat = 50
-    static let minTileSize: CGFloat = 38
+    static let minTileSize: CGFloat = 25.5
     static let maxTileSize: CGFloat = 54
     static let validate = false
     static let autofill = true
